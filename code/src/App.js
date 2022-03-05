503227b139617b100c645042b96cef210bc675bd
Umair Khan <umair.khan@toptal.com>
2022-03-05 19:26:13 +0500

    Run style

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi {process.env.REACT_APP_NAME}
-        </p>
+        <p>Hi {process.env.REACT_APP_NAME}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
